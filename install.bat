1981fb25ce5cbe1ba4ca3c0fc20abd72ab7fa357
yefan <yefansky@gmail.com>
2025-02-05 15:01:08 +0800

    降低对vscode的版本依赖

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-npm install --save-dev @types/vscode ^
+npm install @types/vscode@1.70.0 --save-dev ^
 && npm install --save-dev @types/estree ^
 && npm install --save-dev standard-version ^
 && npm install vsce -g --save vsce
