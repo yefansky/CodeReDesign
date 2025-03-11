eb6c069edc83904d24931f0a20955b0c0a1492ff
yefan <yefansky@gmail.com>
2025-03-11 15:15:56 +0800

    fix:测试工程编译错误

--- src/test/fixapi.test.ts
+++ src/test/fixapi.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as cvbMgr from '../cvbManager';
-
+import * as fs from "fs";
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
