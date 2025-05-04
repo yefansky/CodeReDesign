4b582fc1ef9e3272385015f984be6e34a3b880f3
yefan <yefansky@gmail.com>
2025-05-04 22:39:05 +0800

    fix:修复TCVB某些情况下会解析失败的情况, 且添加单元测试用例

--- src/test/fixapi.test.ts
+++ src/test/fixapi.test.ts
@@ -1,13 +1,35 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-import * as cvbMgr from '../cvbManager';
-import * as fs from "fs";
+﻿import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Cvb, TCVB, mergeCvb } from '../cvbManager'; // Adjust path to your models
 
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+suite('MergeCvb Test Suite', () => {
+  test('mergeCvb produces different content from oldCvb', () => {
+    const oldCvbFilePath = path.join(__dirname, '../../testdata/testfix_input_1_cvb.cvb');
+    const tcvbFilePath = path.join(__dirname, '../../testdata/testfix_input_1_tcvb.txt');
 
-    test('testmerge', () => {
-        const cvbFilePath = "test/";
-        let cvbContent = fs.readFileSync(cvbFilePath, 'utf-8');
-    });
+    const oldCvbContent = fs.readFileSync(oldCvbFilePath, 'utf-8');
+    const tcvbContent = fs.readFileSync(tcvbFilePath, 'utf-8');
+
+    const oldCvb = new Cvb(oldCvbContent);
+    const tcvb = new TCVB(tcvbContent);
+
+    const resultCvb = mergeCvb(oldCvb, tcvb);
+
+    console.log(resultCvb.toString());
+
+    // Remove ## META to ## END_META section from both strings
+    const removeMetaSection = (content: string): string => {
+        const metaRegex = /## META[\s\S]*?## END_META\n?/g;
+        return content.replace(metaRegex, '');
+    };
+    
+    const processedResult = removeMetaSection(resultCvb.toString());
+    const processedOldCvb = removeMetaSection(oldCvb.toString());
+
+    //const processedResult = resultCvb.toString();
+    //const processedOldCvb = oldCvb.toString();
+
+    assert.notStrictEqual(processedResult, processedOldCvb);
+  });
 });
